fad65cfa227ca5351e1dec7cef629f073aaff384
Rene <rmbanguka@gmail.com>
2023-11-10 23:59:00 +0100

    Initial Commit

---
+++ Introappen/IntroappenApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroappenApp.swift
+//  Introappen
+//
+//  Created by Rene Mbanguka on 2023-11-10.
+//
+
+import SwiftUI
+
+@main
+struct IntroappenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
