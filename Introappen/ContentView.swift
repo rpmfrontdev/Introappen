dfe953e3ecc24dc18a5fe82b5c5cb82f952c9dda
Rene <rmbanguka@gmail.com>
2023-11-11 00:57:24 +0100

    Initial Commit

--- Introappen/ContentView.swift
+++ Introappen/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
     var body: some View {
         VStack {
+            
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+            
+            
             Text("Hello, world!")
+                .font(.title)
+                .foregroundColor(Color("fancycolor"))
+            
+            
         }
         .padding()
     }
